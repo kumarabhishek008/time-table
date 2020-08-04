1c39cfa3fe83888224c0d8caa5f0050e3ac4454a
abhishek <kumar008abhishek>
2020-08-04 07:54:12 +0530

    added static

--- usertt/urls.py
+++ usertt/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index,name='index'),
     path('login',views.login,name='login'),
@@ -9,3 +11,5 @@ urlpatterns = [
     
     
 ]
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
